4a72c11905c099b4935a50b667e54dd3fa3f5210
Victor Geislinger <9027783+MrGeislinger@users.noreply.github.com>
2022-04-12 07:05:18 -0700

    Shell script to get clean data

---
+++ notebook/modularize/get_clean_data.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+echo 'Loading data...'
+python load_data.py
+echo
+echo 'Splitting data into train & test X & y sets...'
+python train_test_split.py -d data.csv
+echo
+echo 'Cleaning training and training data...'
+python clean_data.py
